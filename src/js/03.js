b928317f11abf9764ea0188d0af0b326f2392d37
Piotr Frontczak <piotrf2023@gmail.com>
2024-04-14 21:52:46 +0200

    updated

--- src/js/03.js
+++ src/js/03.js
@@ -19,3 +19,11 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
